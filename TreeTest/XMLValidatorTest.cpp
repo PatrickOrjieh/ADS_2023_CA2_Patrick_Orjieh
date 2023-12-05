18d03d491af17affa7d177976487c504d5f2b1dd
Patrick Orjieh <orjiehpatrick@gmail.com>
2023-12-05 12:21:22 +0000

    Add initial tests for XML validation

---
+++ TreeTest/XMLValidatorTest.cpp
@@ -0,0 +1,26 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../XMLValidator/XMLValidator.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace XMLValidatorTest
+{
+	TEST_CLASS(TestXMLValidator)
+	{
+	public:
+        XMLValidator validator;
+
+        TEST_METHOD(TestMethod_ValidateEmptyFile) {
+            Assert::IsFalse(validator.validateFile("empty.xml"), L"An empty XML should be invalid.");
+        }
+
+        TEST_METHOD(TestMethod_ValidateSimpleValidXMLFile) {
+            Assert::IsTrue(validator.validateFile("simple_valid.xml"), L"A simple valid XML with a single root should be valid.");
+        }
+
+        TEST_METHOD(TestMethod_ValidateNestedValidXMLFile) {
+            Assert::IsTrue(validator.validateFile("nested_valid.xml"), L"A nested valid XML should be valid.");
+        }
+
+	};
+}
